3e60b74296d52a596b1797c688846f31e0676079
tsukasa inoue <inoue.tsukasa.4@gmail.com>
2026-03-09 12:55:20 +0900

    chore: update

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,41 +1,5 @@
 return {
 	{
-		'neovim/nvim-lspconfig',
-		ft = { 'go', 'gomod' },
-		config = function()
-			require('lspconfig').gopls.setup({
-				settings = {
-					gopls = {
-						analyses = {
-							nilness = true,
-							unusedparams = true,
-							unusedwrite = true,
-							useany = true,
-						},
-						staticcheck = true,
-						gofumpt = true,
-						completeUnimported = true,
-						semanticTokens = true,
-						codelenses = {
-							gc_details = true,
-							generate = true,
-							run_govulncheck = true,
-							test = true,
-							tidy = true,
-						},
-						hints = {
-							assignVariableTypes = true,
-							compositeLiteralFields = true,
-							constantValues = true,
-							parameterNames = true,
-						},
-					},
-				},
-			})
-		end,
-	},
-
-	{
 		'stevearc/conform.nvim',
 		ft = 'go',
 		opts = {
