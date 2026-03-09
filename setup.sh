44a4447139fae995753f1be686412de01550779f
tsukasa inoue <inoue.tsukasa.4@gmail.com>
2026-03-09 12:54:53 +0900

    feat: replace starship with built-in zsh prompt

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ echo "Setting up symlinks..."
 # ~/.config symlinks
 ln -sf "$DOTFILES/nvim" ~/.config/nvim
 ln -sf "$DOTFILES/ghostty" ~/.config/ghostty
-ln -sf "$DOTFILES/starship/starship.toml" ~/.config/starship.toml
+
 
 # Home directory symlinks
 ln -sf "$DOTFILES/zsh/zshrc" ~/.zshrc
