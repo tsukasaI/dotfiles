3beb39a12f93511b38b4a943ac05bef0bccd4a2e
tsukasa inoue <inoue.tsukasa.4@gmail.com>
2026-03-20 18:59:21 +0900

    feat(nvim): update settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,12 +9,19 @@ vim.opt.tabstop = 2             -- タブ幅
 vim.opt.shiftwidth = 2          -- インデント幅
 vim.opt.expandtab = true        -- スペースに展開
 vim.opt.smartindent = true      -- スマートインデント
+vim.opt.ignorecase = true       -- 検索時大文字小文字無視
 vim.opt.smartcase = true        -- 大文字含むと区別
 vim.opt.splitright = true       -- 右に分割
 vim.opt.splitbelow = true       -- 下に分割
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.mouse = ''
 vim.opt.signcolumn = 'yes'
+vim.opt.undofile = true         -- 永続undo
+vim.opt.scrolloff = 10          -- スクロール余白
+vim.opt.showmode = false        -- lualineに任せる
+vim.opt.updatetime = 250        -- CursorHold高速化
+vim.opt.inccommand = 'split'    -- 置換リアルタイムプレビュー
+vim.opt.breakindent = true      -- 折り返しインデント維持
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -27,6 +34,19 @@ vim.diagnostic.config({
   float = { border = 'rounded' },
 })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
+
+-- Keymaps
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Focus left window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Focus lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Focus upper window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Focus right window' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('x', '<', '<gv')
+vim.keymap.set('x', '>', '>gv')
+vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move line up' })
 
 -- LSP
 vim.lsp.config('gopls', {
--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,5 +1,13 @@
 return {
   'ibhagwan/fzf-lua',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { '<leader>sf', '<cmd>FzfLua files<cr>', desc = 'Search files' },
+    { '<leader>sg', '<cmd>FzfLua live_grep<cr>', desc = 'Search by grep' },
+    { '<leader>sb', '<cmd>FzfLua buffers<cr>', desc = 'Search buffers' },
+    { '<leader>sd', '<cmd>FzfLua diagnostics_document<cr>', desc = 'Search diagnostics' },
+    { '<leader><leader>', '<cmd>FzfLua buffers<cr>', desc = 'Find buffers' },
+    { '<leader>/', '<cmd>FzfLua grep_curbuf<cr>', desc = 'Search in buffer' },
+  },
   opts = {},
 }
