b5ecccb0a3227ef3e5e73d8dd0e8c15f569a4589
tsukasa inoue <65926267+tsukasaI@users.noreply.github.com>
2026-04-06 14:51:45 +0900

    chore: update

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -2,4 +2,28 @@ return {
 	'mrcjkb/rustaceanvim',
 	version = '^8',
 	lazy = false,
+	init = function()
+		---@type RustaceanOpts
+		vim.g.rustaceanvim = {
+			server = {
+				default_settings = {
+					['rust-analyzer'] = {
+						check = {
+							command = 'check',
+							extraArgs = { '--target-dir', 'target/ra-check' },
+						},
+						cargo = {
+							buildScripts = { enable = true },
+						},
+						procMacro = { enable = true },
+						completion = {
+							limit = 30,
+						},
+						cachePriming = { enable = false },
+						lru = { capacity = 64 },
+					},
+				},
+			},
+		}
+	end,
 }
